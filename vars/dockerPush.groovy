cfdaf2e74830bfe41348f2839b4e84ad0ef8c28d
Bithun Chatterjee <bithun.chatterjee@gmail.com>
2026-01-05 02:20:21 +0100

    Refactor dockerPush function for improved credentials handling

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,6 +1,13 @@
-def call(String Project, String ImageTag, String dockerhubuser){
-  withCredentials([usernamePassword(credentialsId: 'dockerHubCred', passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]) {
-      sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
+def call(String imageName, String imageTag, String credentialsId) {
+  withCredentials([usernamePassword(
+    credentialsId: credentialsId,
+    usernameVariable: 'DOCKERHUB_USER',
+    passwordVariable: 'DOCKERHUB_PASS'
+  )]) {
+    sh """
+      echo "\$DOCKERHUB_PASS" | docker login -u "\$DOCKERHUB_USER" --password-stdin
+      docker tag ${imageName}:${imageTag} "\$DOCKERHUB_USER/${imageName}:${imageTag}"
+      docker push "\$DOCKERHUB_USER/${imageName}:${imageTag}"
+    """
   }
-  sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
 }
